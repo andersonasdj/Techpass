24481a2714aa9f6702376661209b019f6a2eb2e5
Anderson de Araujo <andersonasdj@gmail.com>
2023-07-27 00:37:59 -0300

    Ajustes de DB file

--- src/main/java/br/com/techgold/security/SecurityFilter.java
+++ src/main/java/br/com/techgold/security/SecurityFilter.java
@@ -17,7 +17,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
-		System.out.println("teste");
+		//System.out.println("teste");
 		filterChain.doFilter(request, response);
 	}
 	
